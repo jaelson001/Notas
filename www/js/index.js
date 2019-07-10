b04e3b69866185d4afa9a67b3ea84a8950da131b
j001942025 <jaelson.araujo@linksearch.com.br>
2019-07-10 13:54:10 -0300

    inserida função de salvamento local

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,54 @@
+var contador = (localStorage.length)/3;
 
+window.onload = function(){
+    for (var i = 1; i <= contador; i++) {
+        var popup_titulo = localStorage.getItem(i+"_t");
+        var popup_conteudo = localStorage.getItem(i+"_c");
+        var today = localStorage.getItem(i+"_d");
 
-var contador = 0;
+        var nota = document.createElement("DIV");
+        var titulo_nota = document.createElement("H2");
+        var conteudo_nota = document.createElement("P");
+        var data = document.createElement("DIV");
+        var del = document.createElement("BUTTON");
+
+        nota.classList.add("note");
+        nota.classList.add("bounceIn");
+        nota.setAttribute("id",i);
+        titulo_nota.classList.add("note_title");
+        conteudo_nota.classList.add("note_content");
+        del.classList.add("btn");
+        del.classList.add("note_del");
+        data.classList.add("date");
+
+        titulo_nota.innerHTML = popup_titulo;
+        conteudo_nota.innerHTML = popup_conteudo;
+        del.innerHTML = "excluir";
+        data.innerHTML = today;
+
+        del.addEventListener("click", function(){
+            var id = this.parentNode.getAttribute("id");
+            document.getElementById(id).outerHTML = "";
+
+            // REMOVER LOCALMENTE
+            localStorage.removeItem(id+"_t");
+            localStorage.removeItem(id+"_c");
+            localStorage.removeItem(id+"_d");
+        });
+
+        nota.appendChild(titulo_nota);
+        nota.appendChild(conteudo_nota);
+        nota.appendChild(data);
+        nota.appendChild(del);
+
+        nota.addEventListener("click", function(){
+            this.classList.toggle("focus");
+        });
+        
+        document.getElementById("content").appendChild(nota);
+    }
+}
+
 var app = {
 
     // Application Constructor
@@ -42,11 +90,13 @@ var app = {
             var data = document.createElement("DIV");
             var del = document.createElement("BUTTON");
 
+            contador++;
             nota.classList.add("note");
             nota.classList.add("bounceIn");
-            contador++;
             nota.setAttribute("id",contador);
-            
+            titulo_nota.setAttribute("id",contador+"_t");
+            conteudo_nota.setAttribute("id",contador+"_c");
+            data.setAttribute("id",contador+"_d");
             titulo_nota.classList.add("note_title");
             conteudo_nota.classList.add("note_content");
             del.classList.add("btn");
@@ -67,8 +117,18 @@ var app = {
             del.addEventListener("click", function(){
                 var id = this.parentNode.getAttribute("id");
                 document.getElementById(id).outerHTML = "";
+
+                // REMOVER LOCALMENTE
+                localStorage.removeItem(id+"_t");
+                localStorage.removeItem(id+"_c");
+                localStorage.removeItem(id+"_d");
             });
             
+            // SALVAR LOCALMENTE
+            localStorage.setItem(contador+"_t", popup_titulo);
+            localStorage.setItem(contador+"_c", popup_conteudo);
+            localStorage.setItem(contador+"_d", today);
+
             nota.appendChild(titulo_nota);
             nota.appendChild(conteudo_nota);
             nota.appendChild(data);
@@ -83,7 +143,7 @@ var app = {
             document.getElementById("popup_note_title").value = "";
             document.getElementById("popup_note_description").value = "";
             document.getElementById("popup_new_note").style.left = "100%";
-
+            
         });
     }
 };
